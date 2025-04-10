99a9a2a33b1593ae49cb70c59f5c806719fd3980
mohamadanasfattoum <samaanas30@gmail.com>
2025-04-10 14:31:25 +0200

    created design models mit relations

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask , render_template
-from .models import db
+from .models import db, Book, Author, Review
 app = Flask(__name__)
 
 # configuration SQLALCHEMY
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-# design models
+# design models mit relations
 
 from flask_sqlalchemy import SQLALchemy
 
@@ -7,14 +7,27 @@ db = SQLALchemy()
 
 
 class Author(db.models):
-    pass
-
-
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Stringe(100),nullable=False)
+    
+    # ein Author hat auch books
+    books = db.relationship('Book',backref='author',lazy=True)
 
 
 class Book(db.models):
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Stringe(100),nullable=False)
+
+    # jedes Book hat ein Author id
+    author_id = db.Column(db.Integer,db.ForeignKey('auther.id'),nullable=False)
+
+    # ein book hat reviews
+    reviews = db.relationship('Review',backref='book',lazy=True)
 
 
 class Review (db.models):
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text,nullable=False)
+
+    # review hat auch book id für welches book es gehört
+    book_id = db.Column(db.Integer,db.ForeignKey('book.id'),nullable=False)
