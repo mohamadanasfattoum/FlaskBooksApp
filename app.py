a0d5a2c6cefd96b77551e77e9efc1a1b4dc95036
mohamadanasfattoum <samaanas30@gmail.com>
2025-04-11 12:09:48 +0200

    add celery Configuration

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
 from flask import Flask , render_template, request, redirect, url_for
+from celery import Celery, Task
 import os
 from models import db, Book, Author, Review
+
+
 app = Flask(__name__)
+
+def celery_init_app(app: Flask) -> Celery:
+    class FlaskTask(Task):
+        def __call__(self, *args: object, **kwargs: object) -> object:
+            with app.app_context():
+                return self.run(*args, **kwargs) 
+
+    celery_app = Celery(app.name, task_cls=FlaskTask)
+    celery_app.config_from_object(app.config["CELERY"])
+    celery_app.set_default()
+    app.extensions["celery"] = celery_app
+    return celery_app
+
+app.config.from_mapping(
+    CELERY=dict(
+        broker_url="redis://localhost",
+        result_backend="redis://localhost",
+        task_ignore_result=True,
+    ),
+)
+celery_app = celery_init_app(app)
+
 
 # Configuration for SQLAlchemy - database will be created in current folder
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -80,7 +105,7 @@ def book_detail(id):
 
 
 @app.route('/search',methods=['POST','GET'])
-def search_books():
+def search_books(): 
     if request.method == 'POST':
         query = request.form['query']
         # print(f'query: {query}') # to testing
--- befehle.md
+++ befehle.md
@@ -3,3 +3,5 @@ pip install flask flask-sqlalchemy flask-migrate
 source ../bin/activate
 
 pip freeze > requirement.txt
+
+pip install celery redis
